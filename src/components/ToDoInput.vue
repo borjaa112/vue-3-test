56b633eb5b20657d283e7e9812c1717117919381
Borja Ferrer <borjetaa27@gmail.com>
2022-11-30 15:03:02 +0100

    added autoclear of input on submit event

--- src/components/ToDoInput.vue
+++ src/components/ToDoInput.vue
@@ -2,10 +2,14 @@
 import { ref } from "vue";
 
 const todoInput = ref();
+
+function clearInput() {
+  todoInput.value = "";
+}
 </script>
 <template>
   <div>
-    <form @submit.prevent>
+    <form @submit.prevent="clearInput">
       <input type="text" placeholder="hacer la cama" v-model="todoInput" />
       <button type="submit" @click="$emit('addToList', todoInput)">
         &#10003;
